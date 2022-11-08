8ad33ccb2f7a6c005fc1731afae4730ed363ef83
kayashimanaho <116855181+kayashimanaho@users.noreply.github.com>
2022-11-08 05:23:44 +0000

    indexページの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-    get 'books/new'
     get 'top' => 'homes#top'
     post 'books' => 'books#create'
     get 'books' => 'books#index'
-    get 'books/:id' => 'books#show'
+    get 'books/:id' => 'books#show',as:'book'
     get 'books/edit'
     get '/top' => 'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
