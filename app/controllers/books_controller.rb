1e9de98a4aabaee65f7488193bcefbfb66b94e72
kayashimanaho <116855181+kayashimanaho@users.noreply.github.com>
2022-11-09 05:24:23 +0000

    フラッシュメッセージstart

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -11,17 +11,28 @@ class BooksController < ApplicationController
     
     def index
         @books = Book.all
+        @book = Book.new
     end
     
     def show
-       @book = books.find(params[:id])  
+       @book = Book.find(params[:id])  
     end
     
     def edit
+        @book = Book.find(params[:id])
     end
     
+    def update
+       book = Book.find(params[:id])
+       book.update(book_params)
+       redirect_to book_path(book.id)  
+    end
     
-    
+    def destroy
+        book = Book.find(params[:id])
+        book.destroy
+        redirect_to '/books'
+    end
     
     private
      def book_params
